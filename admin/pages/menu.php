c8c0ac929353302fc33d4bce2167969b801f12d4
Davi ferreira de sena <davifsena2@gmail.com>
2022-04-02 07:48:33 -0300

    mvc experiencias iniciado

--- admin/pages/menu.php
+++ admin/pages/menu.php
@@ -29,6 +29,12 @@
                         <p>Habilidades</p>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a href="../experiencia/experiencia.php" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Experiências de trabalho</p>
+                    </a>
+                </li>
 
         </li>
 
